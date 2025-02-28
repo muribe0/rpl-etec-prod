213125177d67870b79d253e0b7cfac6480bd0d27
muribe <muribe@fi.uba.ar>
2025-02-28 00:16:03 -0300

    add Celery configuration and expose celery_app in __init__.py

--- rpl/settings.py
+++ rpl/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CELERY
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
