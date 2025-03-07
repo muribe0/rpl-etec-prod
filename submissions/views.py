7ba68214b86597167b91b173159bee2f093bdbee
muribe <muribe@fi.uba.ar>
2025-03-07 13:51:43 -0300

    removed early views and urls

--- submissions/views.py
+++ submissions/views.py
@@ -7,28 +7,12 @@ from django.db import transaction
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
+from excercises.models import Excercise
 from .forms import CodeSubmissionForm
 from .models import CodeSubmission
 
-from .tasks import test_code, logger
+from .tasks import test_code
 
-
-def index(request):
-    if request.method == "GET":
-        return render(request,
-                      "submissions/index.html",
-                      {"form": CodeSubmissionForm()})
-
-    form = CodeSubmissionForm(request.POST)
-    if not form.is_valid():
-        return render(request,
-                      "submissions/index.html",
-                      {"form": form})
-
-    context = {
-        'form': form,
-    }
-    return render(request, "submissions/index.html", context)
 
 
 @require_POST
@@ -39,19 +23,32 @@ def submit_api(request):
     :param request: POST request with JSON data in its body. The JSON data must contain a 'code' key with the code to test.
     :return: JSON response with the submission_id and task_id of the task that was created to test the code.
     """
+    # TODO: check authenticity of request (correct user, excercise, etc)
     try:
         data = json.loads(request.body)
         code = data.get('code', "")
+        excercise_pk = data.get('excercise_pk', None)
 
-        submission = CodeSubmission.objects.create(code=code)
+        if not excercise_pk:
+            return JsonResponse({
+                "error": "Excercise pk not provided"
+            }, status=400)
 
-        unique_id = data.get('unique_id', submission.pk)
+        try:
+            with transaction.atomic():
+                excercise = Excercise.objects.get(pk=excercise_pk)
+                submission = CodeSubmission.objects.create(excercise=excercise,
+                                                   code=code)
+        except Exception as e:
+            return JsonResponse({
+                "error": str(e)
+            }, status=500)
 
         # async task
         res = test_code.delay(submission.pk).id
-
+        print(res)
         return JsonResponse({
-            "submission_id": unique_id,
+            "submission_id": submission.pk,
             "task_id": res
         }, status=200)
     except json.JSONDecodeError:
@@ -64,8 +61,7 @@ def submit_api(request):
             "error": str(e)
         }, status=500)
     finally:
-        print(CodeSubmission.objects.all().first().result)
-
+        pass
 
 def results_api(request, task_id):
     """
@@ -88,51 +84,3 @@ def results_api(request, task_id):
         "results": result
     }, status=200)
 
-
-def auto_submit(request, num):
-    code_0 = """
-def suma(x, y):
-    return suma(x,y)
-    """
-
-    code_1 = """
-def suma(x, y):
-    c = 0
-    for i in range(100000000000000000):
-        c += i
-    return x + y
-    """
-
-    code_2 = """
-def suma(x,y): 
-    return x + y
-    """
-
-    codes = [code_0, code_1, code_2]
-
-    submission = CodeSubmission.objects.create(code=codes[num])
-    
-    test_code.delay(submission.pk)
-
-    return render(request, "submissions/submit.html", {'form': CodeSubmissionForm()})
-
-
-def submit(request):
-    if request.method == "GET":
-        return render(request,
-                      "submissions/submit.html",
-                      {"form": CodeSubmissionForm()})
-
-    form = CodeSubmissionForm(request.POST)
-
-    if not form.is_valid():
-        return render(request,
-                      "submissions/submit.html",
-                      {"form": form})
-
-    submission = CodeSubmission.objects.create(code=form.cleaned_data['code'])
-
-    # async task
-    test_code.delay(submission.pk)
-
-    return render(request, "submissions/submit.html", {'form': form})
