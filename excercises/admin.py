d30451a7a8db1b5205b00cb4815dcb4fbb843b69
muribe <muribe@fi.uba.ar>
2025-03-07 13:49:12 -0300

    change some columns from admin views

--- excercises/admin.py
+++ excercises/admin.py
@@ -3,24 +3,26 @@ from .models import Course, Unit, Excercise, File
 
 # Register your models here.
 
-@admin.register(File)
+# @admin.register(File)
 class FileAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name',)
     search_fields = ('name',)
     ordering = ('pk',)
 
 
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'group')
+    list_display = ('pk', 'title')
     search_fields = ('title', 'slug')
     ordering = ('pk',)
     prepopulated_fields = {'slug': ('title',)}
 
 
+
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'course')
+    list_display = ('pk', 'title', 'course__title')
     search_fields = ('title', 'slug')
     ordering = ('pk',)
     prepopulated_fields = {'slug': ('title',)}
