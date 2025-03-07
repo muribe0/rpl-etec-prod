f75dbaad9fc4d8a3bee269097012090820ba87d1
muribe <muribe@fi.uba.ar>
2025-03-07 13:52:55 -0300

    add get_urls, made groups a ManyToManyField in Course, added default manager to Unit

--- submissions/models.py
+++ submissions/models.py
@@ -1,18 +1,20 @@
 from django.db import models
-# from excercises.models import Excercise
+from excercises.models import Excercise
 
 # Create your models here.
 class CodeSubmission(models.Model):
-
+    # student = models.ForeignKey(Student,
+                                # on_delete=models.CASCADE,
+                                # related_name="user_submissions")
     created_at = models.DateTimeField(auto_now_add=True)
 
-    # excercises = models.ForeignKey(Excercise,
-    #                                on_delete=models.CASCADE,
-    #                                related_name="submissions")
+    excercise = models.ForeignKey(Excercise,
+                                  on_delete=models.CASCADE,
+                                  related_name="submissions",
+                                  )
     succes = models.BooleanField(null=True, blank=True)
     code = models.TextField(max_length=100000, default="")
     result = models.TextField(default="Codigo fallido o aun no testeado")
 
 
     objects = models.Manager()
-# docker compose exec web python /code/manage.py migrate
