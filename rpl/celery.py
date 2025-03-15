365b8745825f72e42830298a3c905f00e6e6bcbe
muribe <muribe@fi.uba.ar>
2025-03-15 18:19:38 -0300

    now celery clears tasks every time the app initiates

--- rpl/celery.py
+++ rpl/celery.py
@@ -5,8 +5,17 @@ from rpl.settings import CELERY_BROKER_URL
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rpl.settings')
 
-app = Celery('rpl', broker=CELERY_BROKER_URL)
+app = Celery('rpl', broker=CELERY_BROKER_URL) # can remove broker if celery faisl
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
+# Force reloading of all tasks
+app.tasks.clear()
+
+app.autodiscover_tasks(packages=['submissions'])
+
+
+# debugging task discovery
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
