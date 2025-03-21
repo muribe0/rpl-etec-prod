025b8e95a2869d53c60225fc48be80019e0b683a
muribe <muribe@fi.uba.ar>
2025-03-20 23:35:02 -0300

    add related_name to Profile user field for improved reverse access

--- account/models.py
+++ account/models.py
@@ -8,7 +8,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='profile'
     )
     date_of_birth = models.DateField(blank=True, null=True)
     dni = models.CharField(max_length=9, unique=True)
