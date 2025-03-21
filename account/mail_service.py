3a56b0b13323b3fe3f1bd594ddee348ed7988bc2
muribe <muribe@fi.uba.ar>
2025-03-20 23:34:55 -0300

    update send_confirmation_email function to include detailed docstring and modify message format

--- account/mail_service.py
+++ account/mail_service.py
@@ -3,14 +3,19 @@ from django.conf import settings
 
 
 def send_confirmation_email(user):
-    email = user.email
+    """
+    Sends a confirmation email to the user with a link to confirm the registration.
+    Generates a token based on the user's username hash.
+    The token is sent as a parameter in the confirmation link.
+    The idea is for the token to be sent to the user's inbox and is a way to verify that the user has access to the email.
+    :param user: The user to send the email to
+    """
     user_pk = user.pk
     token = int(hash(user.username))
     token = token if token > 0 else -token
     subject = 'Confirmación de registro'
-    message = f'Por favor, confirma tu registro en el siguiente enlace: ' \
-              f'https://{settings.DOMAIN}/account/register/confirm/{user_pk}/{token}/'
+    message = f'Por favor, confirma tu registro ingresando en el siguiente enlace: ' \
+              f' {settings.DOMAIN}/account/register/confirm/{user_pk}/{token}/' \
+              f'Si no te registraste, ignora este mensaje.'
 
-    return send_mail(subject, message, None, ['rpl.etec@gmail.com'], fail_silently=False)
-
-
+    return send_mail(subject, message, None, [user.email], fail_silently=False)
